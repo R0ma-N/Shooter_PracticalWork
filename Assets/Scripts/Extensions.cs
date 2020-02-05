3c38a4fd6a1f611800771d547ee280b129412ae2
Roman <x6wert@gmail.com>
2020-02-06 00:07:52 +0300

    save+

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Shooter
+{
+	public static partial class Extensions
+	{
+		public static Bounds GrowBounds(this Bounds a, Bounds b)
+		{
+			var max = Vector3.Max(a.max, b.max);
+			var min = Vector3.Min(a.min, b.min);
+
+			a = new Bounds((max + min) * 0.5f, max - min);
+
+			return a;
+		}
+
+		public static bool TryBool(this string self)
+		{
+			return Boolean.TryParse(self, out var res) && res;
+		}
+
+		public static float TrySingle(this string self)
+		{
+			if (Single.TryParse(self, out var res))
+			{
+				return res;
+			}
+			return 0;
+		}
+	}
+}
