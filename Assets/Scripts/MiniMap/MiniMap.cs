ba1afc3db45027d8dcb94a6701653c8a0b559c9f
R0ma-N <X6wert@gmail.com>
2020-02-12 12:59:42 +0300

    Revert "Revert "minimap""
    This reverts commit 0f255ebafd51a93876a87423d51e6ba0e9068b88.

---
+++ Assets/Scripts/MiniMap/MiniMap.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Shooter
+{
+	public class MiniMap : MonoBehaviour
+	{
+		private Transform _player;
+		float storedShadoDistance;
+
+		private void OnPreRender()
+		{
+			storedShadoDistance = QualitySettings.shadowDistance;
+			QualitySettings.shadowDistance = 0;
+		}
+
+		private void OnPostRender()
+		{
+			QualitySettings.shadowDistance = storedShadoDistance;
+		}
+
+		private void Start()
+		{
+			_player = Camera.main.transform;
+			var rt = Resources.Load<RenderTexture>("Minimap");
+			GetComponent<Camera>().targetTexture = rt;
+		}
+
+		private void LateUpdate()
+		{
+			var newPosition = _player.position;
+			newPosition.y = transform.position.y;
+			transform.position = newPosition;
+			transform.rotation = Quaternion.Euler(90, _player.eulerAngles.y, 0);
+		}
+	}
+
+}
