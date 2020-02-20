492db446ea11602d105253fc05b88dddaae36b4e
Roman <x6wert@gmail.com>
2020-02-20 23:10:49 +0300

    Anim-fix

--- Assets/Scripts/Controllers/WeaponController.cs
+++ Assets/Scripts/Controllers/WeaponController.cs
@@ -29,14 +29,16 @@ namespace Shooter
                     _activeWeapon.IsReady = _timer.TimeIsUp(_activeWeapon.ShootInterval);
                 }
                 else return;
-                _playerAnimation.SetTrigger("Shoot");
+                _playerAnimation.SetBool("Shoot", true);
+                Debug.Log("fire");
             }
             else if (Input.GetKeyUp(_fire))
             {
                 _activeWeapon.IsReady = true;
                 _timer.DistTime = 0;
-
-                if(_activeWeapon is Flamethrower)
+                _playerAnimation.SetBool("Shoot", false);
+                Debug.Log("STOPfire");
+                if (_activeWeapon is Flamethrower)
                 {
                     _activeWeapon.StopFire();
                 }
--- Assets/Scripts/MiniMap/MiniMap.cs
+++ Assets/Scripts/MiniMap/MiniMap.cs
@@ -20,18 +20,18 @@ namespace Shooter
 
 		private void Start()
 		{
-			_player = Camera.main.transform;
+			//_player = Camera.main.transform;
 			var rt = Resources.Load<RenderTexture>("Minimap");
 			GetComponent<Camera>().targetTexture = rt;
 		}
 
-		private void LateUpdate()
-		{
-			var newPosition = _player.position;
-			newPosition.y = transform.position.y;
-			transform.position = newPosition;
-			transform.rotation = Quaternion.Euler(90, _player.eulerAngles.y, 0);
-		}
+		//private void LateUpdate()
+		//{
+		//	var newPosition = _player.position;
+		//	newPosition.y = transform.position.y;
+		//	transform.position = newPosition;
+		//	transform.rotation = Quaternion.Euler(90, _player.eulerAngles.y, 0);
+		//}
 	}
 
 }
