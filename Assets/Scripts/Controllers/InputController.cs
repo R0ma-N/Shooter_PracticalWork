f4150508b44d4ff2af988f4aef2208adbfd944b5
R0ma-N <X6wert@gmail.com>
2020-02-22 16:14:05 +0300

    PKM

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -17,6 +17,9 @@ namespace Shooter
         private KeyCode _save = KeyCode.C;
         private KeyCode _load = KeyCode.V;
 
+        float rotationX = 0;
+        float rotationY = 0;
+
         public InputController()
         {
             Cursor.lockState = CursorLockMode.Locked;
@@ -39,11 +42,13 @@ namespace Shooter
             if (Input.GetKeyDown(KeyCode.Mouse1))
             {
                 _camera.SetBool("IsShooting", true);
+                _playerAnimation.SetBool("Aiming", true);
                 Debug.Log("On");
             }
             else if (Input.GetKeyUp(KeyCode.Mouse1))
             {
                 _camera.SetBool("IsShooting", false);
+                _playerAnimation.SetBool("Aiming", false);
                 Debug.Log("Off");
             }
 
@@ -54,7 +59,12 @@ namespace Shooter
             }
 
             float yRot = Input.GetAxis("Mouse X") * sensitivity;
+            float xRot = Input.GetAxis("Mouse Y") * sensitivity;
             _player.localRotation *= Quaternion.Euler(0f, yRot, 0f);
+
+            rotationX += xRot;
+            rotationX = Mathf.Clamp(rotationX, -30, 20);
+            Camera.main.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
 
 
             if (Input.GetKeyDown(_activeFlashLight))
@@ -79,6 +89,23 @@ namespace Shooter
             }
 
 
+        }
+
+        private Quaternion ClampRotationAroundXAxis(Quaternion q)
+        {
+            q.x /= q.w;
+            q.y /= q.w;
+            q.z /= q.w;
+            q.w = 1.0f;
+
+            float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+            //angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+            angleX = Mathf.Clamp(angleX, -90, 90);
+
+            q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+            return q;
         }
     }
 }
